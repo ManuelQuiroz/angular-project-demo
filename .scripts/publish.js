3d8b7c98fb1753241a3eb0ce78acd954d2173ce0
Bram Borggreve <borggreve@gmail.com>
2018-04-07 03:08:02 -0500

    chore(scripts): add logging

--- .scripts/publish.js
+++ .scripts/publish.js
@@ -4,5 +4,8 @@ const { libs } = require('../package.json');
 
 libs.map(lib => {
   const libPath = `${__dirname}/../dist/libs/${lib}`;
+  console.log(` [ publish ] ${lib} from ${libPath}`);
+
   execSync(`cd ${libPath} && npm publish`);
+  console.log(` [ publish ] ${lib}: Done`);
 });
